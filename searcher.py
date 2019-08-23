a0a7b8fbc8c555fccbc6cd2cfdc4f675d07b90e5
TrivialNinja <34358025+TrivialNinja@users.noreply.github.com>
2019-08-23 16:29:09 -0400

    Updated searcher

--- searcher.py
+++ searcher.py
@@ -9,18 +9,20 @@ url_location = "b-search/"
 url_ids = "/k0c760l1700272/"
 
 
-def url_generator(item, min_price, max_price):
+def url_generator(item, min_price, max_price, page_num):
     """
    This function will return url generated based on search item, min and max price
    :param item:
    :param min_price:
    :param max_price:
+   :param page_num:
    :return:
    """
 
     item_spaced = item.replace(" ", "-")
 
-    url_price = "?" + min_price + "__" + max_price
-    url = url_base + url_category + url_location + item_spaced + url_ids + url_price
+    url_price = "?price=" + min_price + "__" + max_price
+    page = "/page-" + page_num
+    url = url_base + url_category + url_location + item_spaced + page + url_ids + url_price
 
     return url
