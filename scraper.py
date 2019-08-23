2cf8877aea6350f21fd863e36a74f52d57176df5
Anas Butt <anasahmad07@outlook.com>
2019-08-23 16:20:10 -0400

    removed writing to csv for now, added date filtering and created an item class

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
+from item import Item
 
 url = "https://www.kijiji.ca/b-cell-phone/gta-greater-toronto-area/c760l1700272?uli=true&ad=offering&price=50__500"
 
@@ -18,27 +19,43 @@ page_soup = bs(page_html, "html.parser")
 # print(page_soup.findAll("div", {"class": "regular-ad"}))
 containers = page_soup.findAll("div", {"class": "regular-ad"})
 
-# file to be saved in
+#file to be saved in
 
-filename = "products.csv"
-f = open(filename, "w")
+#filename = "products.csv"
+#f = open(filename, "w")
 
-headers = "title, price\n"
-f.write(headers)
+#headers = "title, price\n"
+#f.write(headers)
 
+items = []
 for container in containers:
     # print(container.findAll("div", {"class": "price"}))
     # print(container.findAll("div", {"class": "title"}))
 
     title_container = container.findAll("div", {"class": "title"})
     price_container = container.findAll("div", {"class": "price"})
+    date_container = container.findAll("div", {"class": "location"})
 
+    date = date_container[0].span
     product_name = title_container[0].text.strip()  # stripping any white space
     price = price_container[0].text.strip()
 
-    # print(product_name + price)
-    # print("\n")
+    if date:
+        date = date.text.strip()
+        product = Item(product_name, price, date)
+        items.append(product)
 
-    f.write(product_name.replace(",", "|") + ", " + price + "\n")
+    #print(product_name + price)
+    #print("\n")
 
-f.close()
+    #f.write(product_name.replace(",", "|") + ", " + price + "\n")
+
+#f.close()
+
+for object in items:
+    print(object.title)
+    print(object.price)
+    print(object.date)
+    print("\n")
+
+
